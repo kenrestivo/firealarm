2ce672f832d0f9ec061d72323d89099c775f5c88
ken restivo <krestivo@restivo.org>
2012-05-24 02:26:33 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # firealarm
 
-I'm an app. Or maybe I'm a library? I haven't decided yet. 
-
-The choice is up to you!
+A middleware  to alert you if your Noir app stacktraces.
 
 ## Usage
 
-FIXME
+Set up your email/settings in ENV variables (so that it'll work on Heroku, etc).
+
+Compose together which alert mechanisms you want for which mode (:dev or :prod).
+
+In your server.clj, add middleware.
+
+```clojure
+(server/add-middleware (exception-wrapper mode))
+```
+
+## Warnings
+
+Work in progress, this is very rough.
 
 ## License
 
-Copyright © 2012 FIXME
+Copyright © 2012 ken restivo
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- src/firealarm/core.clj
+++ src/firealarm/core.clj
@@ -1,6 +1,64 @@
-(ns firealarm.core)
+(ns firealarm.core
+  (:import java.util.Calendar java.text.SimpleDateFormat )
+  (:require [clj-stacktrace.repl :as cst]
+            [clj-http.client :as client])
+  (:use [clojure.pprint :only [pprint]]))
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+
+
+(defn file-reporter
+  "Simple file dump, overwrites whatever was there previously."
+  [body]
+  (spit "/tmp/noir-error.log" body))
+
+
+(defn post-reporter
+  "Posts the error to another website, so that it can send email.
+   In cases where you might not want your GMail password out in the open."
+  [body]
+  (client/post (get (System/getenv) "FIREALARM_POST_URL")
+               {:form-params {:subject
+                              (str "Error: "
+                                   (get (System/getenv) "FIREALARM_SITENAME"))
+                              :body body
+                              :token (get (System/getenv) "FIREALARM_TOKEN")}}))
+
+
+(defn email-reporter
+  "Uses an email server to send report of error."
+  [body]
+  ;; TODO
+  )
+
+(defn jabber-reporter
+  "Sends error report via jabber"
+  [body]
+  ;; TODO
+  )
+
+
+(defn sms-reporter
+  "Sends SMS with summary of error."
+  [body]
+  ;; TODO
+  )
+
+;;; TODO: allow juxting several of these to compose them nicely
+
+(defn exception-wrapper
+  [mode]
+  (let [reporter (case mode
+                   :dev file-reporter
+                   :prod post-reporter)]
+    (fn wrap-exception
+      [handler]
+      (fn [request]
+        (try
+          (handler request)
+          (catch Throwable e
+            (reporter (str ( -> (Calendar/getInstance) .getTime .toString)
+                           "\n"
+                           (with-out-str  (cst/pst e)
+                             (pprint request))))
+            (throw e)))))))
+
