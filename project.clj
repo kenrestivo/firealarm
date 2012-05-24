8d58e9dc6eb4ecacc3b7b0b701538ebbae040e98
ken restivo <krestivo@restivo.org>
2012-05-24 02:42:18 -0700

    dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject firealarm "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A middleware for sending stacktraces from Noir apps"
+  :url "http://github.com/kenrestivo/firealarm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-stacktrace "0.2.4"]
+                 [clj-http "0.4.1"]])
