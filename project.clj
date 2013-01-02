a30b59488fe1499f95312ea973e23f5583e34788
ken restivo <ken@restivo.org>
2013-01-01 22:58:09 -0800

    Auugh, lein-pedantic.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/kenrestivo/firealarm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-stacktrace "0.2.5"] ;; anything less than this, and cljsbuild gets cranky
                  [clj-http "0.4.1"]])
